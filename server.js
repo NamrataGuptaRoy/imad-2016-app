4ecaf5b127e2633348bca1580a7297282bbdc9b9
NamrataGuptaRoy <namragroy@gmail.com>
2016-11-06 14:54:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-	user: 'pinakinathc',
-	database: 'pinakinathc',
+	user: 'namrataguptaroy',
+	database: 'namrataguptaroy',
 	host: 'db.imad.hasura-app.io',
 	port: '5432',
 	password: process.env.DB_PASSWORD
